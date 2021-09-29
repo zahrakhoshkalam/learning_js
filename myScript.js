4a96b78693e943202370bdb576d0a76a6a5d9e35
Zahra Khoshkalam <zahinakhoshkalam@gmail.com>
2021-09-29 19:35:04 +0330

    javascript_firststep_recorrection

--- myScript.js
+++ myScript.js
@@ -3,17 +3,18 @@ function date_func() {
 }
 function MyFunction() {
   var x, text;
-
-  if (x < 2910000 || x > 2939999) {
-    text = "Input is not Valid";
-    document.getElementById("demo2").innerHTML = text;
-    console.log("By world");
-    return false;
-  } else {
+  x = parseInt(document.getElementById("phone").value);
+  debugger;
+  if (x > 2910000 && x < 2939999) {
     text = "Input is Ok";
     document.getElementById("demo2").innerHTML = text;
     document.getElementById("demo2").style.color = "green";
     console.log("hello world");
     return true;
+  } else {
+    text = "Input is not Ok";
+    document.getElementById("demo2").innerHTML = text;
+    console.log("By world");
+    return false;
   }
 }
